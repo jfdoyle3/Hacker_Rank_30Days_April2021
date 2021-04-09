3302af1ebc7c75db0dd781cc3598f8f4bed0a216
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-04-09 11:04:46 -0400

    completed day 14-17

---
+++ Day16_Exceptions_-_String_to_Integer.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	public static void stringToInteger(String string) {
+		try {
+			int res = Integer.parseInt(string);
+			System.out.println(res);
+
+		} catch (Exception e) {
+			System.out.println("Bad String");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String S = in.next();
+		stringToInteger(S);
+	}
+}
---
+++ Day17_More_Exceptions.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+//Write your code here
+class Calculator {
+	public int power(int n, int p) {
+		double total = 0;
+
+		if (n < 0 || p < 0) 
+			throw new ArithmeticException("n and p should be non-negative");
+		 else 
+			total = (int) Math.pow(n, p);
+		
+
+		return (int) total;
+	}
+}
+
+class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while (t-- > 0) {
+
+			int n = in.nextInt();
+			int p = in.nextInt();
+			Calculator myCalculator = new Calculator();
+			try {
+				int ans = myCalculator.power(n, p);
+				System.out.println(ans);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		in.close();
+	}
+}
