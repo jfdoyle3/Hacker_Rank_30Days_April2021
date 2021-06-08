41e4dc7962f13569b67b045630edf37bb5f87c18
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-06-07 21:15:20 -0400

    refactored Day22_Binary_Search_Trees

--- Day22_Binary_Search_Trees.java
+++ Day22_Binary_Search_Trees.java
@@ -1,97 +1,68 @@
 import java.util.*;
 import java.io.*;
-
-class Node {
-	Node left, right;
-	int data;
-
-	Node(int data) {
-		this.data = data;
-		left = right = null;
-	}
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
 }
+class Solution{
 
-class Solution {
+	public static int getHeight(Node root){
+      //Write your code here
+        Node travLeft, travRight;
+        int leftBranchCount, rightBranchCount, branchRunLeft, branchRunRight;
 
-	public static int getHeight(Node root) {
-		// Write your code here
-		Node travLeft, travRight;
-		int leftBranchCount, rightBranchCount, branchRunLeft, branchRunRight;
+        branchRunLeft = branchRunRight = leftBranchCount = rightBranchCount = 0;
+        travLeft = travRight = root;
 
-		branchRunLeft = branchRunRight = leftBranchCount = rightBranchCount = 0;
-		travLeft = travRight = root;
+        boolean finished = false;
+        int[] results = new int[2];
 
-		boolean finished = false;
-		int[] results = new int[2];
+        if (travLeft.left == null && travRight.right == null)
+            return 0;
 
-		if (travLeft.left == null && travRight.right == null)
-			return 0;
+        do{
+            
+            if (travLeft.left != null){
+                travLeft = travLeft.left;
+                leftBranchCount++;
+            }
+            else if (travLeft.right != null){
+                travLeft = travLeft.right;
+                leftBranchCount++;
+            }
 
-		do {
+            if (travRight.right != null){
+                travRight = travRight.right;
+                rightBranchCount++;
+            }
+            else if (travRight.left != null){
+                travRight = travRight.left;
+                rightBranchCount++;
+            }
 
-			if (travLeft.left != null) {
-				travLeft = travLeft.left;
-				leftBranchCount++;
-			} else if (travLeft.right != null) {
-				travLeft = travLeft.right;
-				leftBranchCount++;
-			}
+            if (travLeft.left == null && travLeft.right == null){
+                results[0] = leftBranchCount;
+                branchRunLeft = 1;
+            }
 
-			if (travRight.right != null) {
-				travRight = travRight.right;
-				rightBranchCount++;
-			} else if (travRight.left != null) {
-				travRight = travRight.left;
-				rightBranchCount++;
-			}
+            if (travRight.left == null && travRight.right == null){
+                results[1] = rightBranchCount;
+                branchRunRight = 1;
+            }
 
-			if (travLeft.left == null && travLeft.right == null) {
-				results[0] = leftBranchCount;
-				branchRunLeft = 1;
-			}
+            if (branchRunLeft == 1 && branchRunRight == 1)
+                finished = true;
 
-			if (travRight.left == null && travRight.right == null) {
-				results[1] = rightBranchCount;
-				branchRunRight = 1;
-			}
+        } while (!finished);
 
-			if (branchRunLeft == 1 && branchRunRight == 1)
-				finished = true;
+        if (results[0] > results[1])
+            return results[0];
+        
+        return results[1];
+    }
 
-		} while (!finished);
-
-		if (results[0] > results[1])
-			return results[0];
-		else
-			return results[1];
-
-	}
-
-	public static Node insert(Node root, int data) {
-		if (root == null) {
-			return new Node(data);
-		} else {
-			Node cur;
-			if (data <= root.data) {
-				cur = insert(root.left, data);
-				root.left = cur;
-			} else {
-				cur = insert(root.right, data);
-				root.right = cur;
-			}
-			return root;
-		}
-	}
-
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		Node root = null;
-		while (T-- > 0) {
-			int data = sc.nextInt();
-			root = insert(root, data);
-		}
-		int height = getHeight(root);
-		System.out.println(height);
-	}
-}
+    public static Node insert(Node root,int data){
